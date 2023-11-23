552c43313b2ffeb6585910a99e7fe3d1e1770abd
AhmedMoee <bkr8gd@virginia.edu>
2023-11-23 16:42:43 -0500

    Some progress to authenticateUser

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.sql.*;
 
 public class LoginController {
 
@@ -21,6 +22,40 @@ public class LoginController {
     protected void handleLogin() {
         String username = usernameField.getText();
         String password = passwordField.getText();
+        if (username.isEmpty() || password.isEmpty()) {
+            messageLabel.setText("Username and password cannot be empty.");
+            return;
+        }
+
+        if (authenticateUser(username, password)) {
+            messageLabel.setText("Login successful!");
+            // Navigate to the next screen or perform other actions upon successful login
+        } else {
+            messageLabel.setText("Invalid username or password.");
+        }
+    }
+
+    private boolean authenticateUser(String username, String password) {
+        // Database URL
+        String dbUrl = "jdbc:sqlite:identifier.sqlite.db";
+
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        try (Connection conn = DriverManager.getConnection(dbUrl);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                // If the query finds a row, the credentials are correct
+                return rs.next();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            // Handle exception - you might want to log this or show an error message
+            return false;
+        }
     }
 
     @FXML
